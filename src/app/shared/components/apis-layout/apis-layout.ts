283d4c661d625f753e168da2563cde617597f680
Davide Passafaro <davide.passafaro@gmail.com>
2026-06-04 23:09:03 +0200

    feat: improve mobile layout

--- src/app/shared/components/apis-layout/apis-layout.ts
+++ src/app/shared/components/apis-layout/apis-layout.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { FUGU_APIS, FuguApi } from '../../../fugu-apis.config';
 
@@ -7,7 +7,19 @@ import { FUGU_APIS, FuguApi } from '../../../fugu-apis.config';
   imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './apis-layout.html',
   styleUrl: './apis-layout.scss',
+  host: {
+    '[class.sidebar-open]': 'menuOpen()',
+  },
 })
 export class ApisLayout {
   readonly apis: FuguApi[] = FUGU_APIS.filter((api) => api.available);
+  readonly menuOpen = signal(false);
+
+  toggleMenu(): void {
+    this.menuOpen.update((v) => !v);
+  }
+
+  closeMenu(): void {
+    this.menuOpen.set(false);
+  }
 }
